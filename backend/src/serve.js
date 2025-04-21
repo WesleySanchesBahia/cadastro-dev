ad1b44a53addc9c73bacf555fe77b83c21054446
Wesley Sanches Bahia <sistema@bruzonmussi.com.br>
2025-04-21 15:30:05 -0300

    configurando backend para subir para producao

--- backend/src/serve.js
+++ backend/src/serve.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import app from "./app.js";
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -11,7 +12,9 @@ mongoose.connect(process.env.DATABASE_URL, {
 }).then(() => {
     console.log("Conectado a o banco")
 }).catch((err) => {
-    console.log(err)
+    console.error('❌ Erro ao conectar no MongoDB:', err.message);
+    process.exit(1);
+
 });
 
 
